23ac0570e0d9e6508b1ceed1c749e06fd8464169
GURSIMRAN KAUR <153198196+Gursimrankaur0612@users.noreply.github.com>
2026-06-29 11:20:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (11.01%) - LeetSync

--- leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,37 +1,37 @@
 class Solution {
     public int search(int[] nums, int target) {
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
 
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) 
+        while(left<=right)
         {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) return mid;
-
-            // Identify the sorted half
-            if (nums[left] <= nums[mid])// Left side is sorted
-             { 
-                if (target >= nums[left] && target < nums[mid])
-                 {
-                    right = mid - 1; // Target is in the left half
-                } 
-                else
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){return mid;}
+            if(nums[left]<=nums[mid])
+            {
+                if(nums[left]<=target && target<nums[mid])
                 {
-                    left = mid + 1;  // Target is in the right half
+                    right=mid-1;
                 }
-            } else
-                { // Right side is sorted
-                if (target > nums[mid] && target <= nums[right])
-                {
-                    left = mid + 1;  // Target is in the right half
-                } 
                 else
                 {
-                    right = mid - 1; // Target is in the left half
+                    left=mid+1;
+                }
+            }
+            else
+            {
+                if(nums[right]>=target && target>nums[mid])
+                {
+                   left=mid+1;
+                }
+                else
+                {
+                   right=mid-1;
                 }
             }
         }
-        return -1;
+
+       return -1;
     }
 }
