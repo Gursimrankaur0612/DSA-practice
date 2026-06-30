c5c203ef3c9cf06d70b920f50fdb41eb7e9a063f
GURSIMRAN KAUR <153198196+Gursimrankaur0612@users.noreply.github.com>
2026-06-30 11:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (18.95%) - LeetSync

---
+++ leetcode/367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num<1)return false;
+
+        int left=1;
+        int right=num;
+      
+
+        while(left<=right)
+        {
+        long mid=left+(right-left)/2;
+        long square=mid*mid;
+            if(square==num)
+                {
+                    return true;
+                }
+            else if(square<num)
+                {
+                    left=(int)mid+1;
+                }
+            else
+            {
+                right=(int)mid-1;
+            }
+            
+        }
+        return false;
+    }
+}
