fd2d5e61c1200bfc4ae79c86a7b556c6c262a3a6
GURSIMRAN KAUR <153198196+Gursimrankaur0612@users.noreply.github.com>
2026-07-20 11:54:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (15.23%) - LeetSync

--- leetcode/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ leetcode/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -30,3 +30,33 @@ class Solution {
         order(root.right,result);
     }
 }
+
+/*import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        
+        if (root != null) {
+            stack.push(root);
+        }
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            
+            
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        
+        return result;
+    }
+}*/
